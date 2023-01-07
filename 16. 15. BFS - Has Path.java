0818ef3b54a97a0fe6b02b64f0c0fe9198a53397
fawkees1101 <abhishekyadav0751@gmail.com>
2023-01-07 22:47:20 +0530

    :white_check_mark:

--- 16. 15. BFS - Has Path.java
+++ 16. 15. BFS - Has Path.java
@@ -1,77 +1,25 @@
 import java.util.*;
-
-// 1971: https://leetcode.com/problems/find-if-path-exists-in-graph/
-
-// Undirected + Unweighted
-class Graph {
-    public ArrayList<Integer>[] adj;
-
-    @SuppressWarnings("unchecked")
-    Graph(int n) {
-        adj = new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<>();
-        }
-    }
-
-    public void addEdge(int src, int dest) {
-        adj[src].add(dest);
-        adj[dest].add(src);
-    }
-
-    public boolean DFS(int src, int dest, boolean[] vis) {
-        if (src == dest)
-            return true;
-        if (vis[src] == true)
-            return false;
-        vis[src] = true;
-
-        // Looping on Adjacency List at Index = Src
-        for (int nbr : adj[src]) {
-            if (DFS(nbr, dest, vis) == true)
-                return true;
-        }
-
-        return false;
-    }
-
-    public boolean DFS(int src, int dest) {
-        boolean[] vis = new boolean[adj.length];
-        return DFS(src, dest, vis);
-    }
-
-    // Time : O(N + E) Same as DFS
-    public boolean BFS(int src, int dest) {
-        Queue<Integer> q = new ArrayDeque<>();
-        boolean[] vis = new boolean[adj.length];
-        q.add(src);
+// https://practice.geeksforgeeks.org/problems/bfs-traversal-of-graph/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+// O(N+E) Time & O(2N) space (Queue and Visited)
+class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> res = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] vis = new boolean[V];
+        q.add(0);
 
         while (q.size() > 0) {
             int front = q.remove();
             if (vis[front] == true)
                 continue;
-            if (front == dest)
-                return true;
-            vis[front] = true;
 
-            for (Integer nbr : adj[front]) {
-                if (vis[nbr] == false) {
-                    q.add(nbr);
-                }
+            vis[front] = true;
+            res.add(front);
+            for (int nbr : adj.get(front)) {
+                q.add(nbr);
             }
         }
-
-        return false;
-    }
-}
-
-class Solution {
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
-        Graph g = new Graph(n);
-        for (int i = 0; i < edges.length; i++) {
-            g.addEdge(edges[i][0], edges[i][1]);
-        }
-
-        return g.BFS(source, destination);
+        return res;
     }
 }
